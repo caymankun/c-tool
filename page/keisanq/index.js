201c61e939fc3b6f0ac28a3ccfa99f49099abef9
caymankun <111965236+caymankun@users.noreply.github.com>
2023-07-06 18:11:50 +0900

    Update index.js

--- page/keisanq/index.js
+++ page/keisanq/index.js
@@ -1,4 +1,9 @@
-        
+input.addEventListener("keydown", event => {
+  if (event.isComposing || event.keyCode === 229) {
+    return;
+}
+  math1();
+});
         let re1, re2, kigou; // 変数を関数の外部で宣言
         math1(); // 初期の問題を生成
         function math1() {
