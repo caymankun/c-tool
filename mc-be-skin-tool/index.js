00a2887e822459cdc36c0be74cbcfef24cf971a6
caymankun <111965236+caymankun@users.noreply.github.com>
2023-07-24 18:07:15 +0900

    Add files via upload

---
+++ mc-be-skin-tool/index.js
@@ -0,0 +1,25 @@
+function getskins(){
+    if( window.event.keyCode == 13 ){
+      enter();
+    }
+  }
+function enter(){
+        const xboxid = document.getElementById('xboxid').value;
+        fetch('https://api.geysermc.org/v2/xbox/xuid/' + xboxid )
+        .then(response => {
+            return response.json()
+        })
+        .then(data1 => {
+            const xuid = data1.xuid
+            fetch('https://api.geysermc.org/v2/skin/' + xuid )
+            .then(response => {
+                return response.json()
+            })
+            .then(data2 => {
+                const texture_id = data2.texture_id
+
+            const Image = document.getElementById('re-skin-image')
+            Image.src = 'https://textures.minecraft.net/texture/' + texture_id ;
+            });
+        });
+};
